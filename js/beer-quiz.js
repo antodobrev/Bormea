fac4d1046c78e027c154041fa7113efc76646c99
antodobrev <anton.dobrev@gmail.com>
2015-12-31 09:58:18 +0200

    further fixes and moving of bubbles

--- js/beer-quiz.js
+++ js/beer-quiz.js
@@ -54,4 +54,14 @@ displaySuccessMessage = function(answer) {
 	}
 }
 
+moveBackground = function() {
+	var y = 0;
+	var element = document.getElementById('main');
+	setInterval(function(){
+		y -= 1;
+		element.style.backgroundPosition = '0 ' + y + 'px';
+	}, 30);
+}
+
+moveBackground();
 displayQuestion();
