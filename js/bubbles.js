aa0627eafdb5c3e89c8d7ab582baf4c2f6ffe75f
antodobrev <anton.dobrev@gmail.com>
2016-01-06 19:13:20 +0200

    move bubbles only up

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,22 +1,13 @@
 moveBackground = function() {
 	var y = 0;
-	var x = 5;
+
 	var movingDown = true;
 	var element = document.getElementById('main');
 	setInterval(function(){
 		y -= 1;
-		if (movingDown) {
-			x -= 0.5;
-		} else {
-			x += 0.5;
-		}
-		if (x == -6) {
-			movingDown = false;
-		};
-		if (x == 6) {
-			movingDown = true;
-		};
-		element.style.backgroundPosition = x + 'px ' + y + 'px';
+
+
+		element.style.backgroundPosition = '0 ' + y + 'px';
 	}, 30);
 }
 
